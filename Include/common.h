afcf53d80f49842b2c33c8a70723f95b34c4cf6b
vr10phoenix <vardhanvachana2005@gmail.com>
2025-10-22 18:39:33 +0530

    Update common.h
    added common constants , added basic structures required according to output

--- Include/common.h
+++ Include/common.h
@@ -1,1 +1,41 @@
+#indef COMMON_H
+#define COMMON_H
+
+#inlcude <vector>
+#include<algorithm>
+#inlcude<cmath>
+#inlcude<limits>
+#include<string>
+
+// requireed Constants : 
+const double EPSILON = 1e-9;
+const int NUM_ORIENTATIONS = 6;
+const double K_COST = 1000.0; // let shipment price for priority packages be 1000.0
+const double DELAY_COST  = 500.0; // delay Cost of any package 
+
+enum class FitStrategy{
+ BEST_VOLUME_FIT,
+ BEST_SIDE_FIT, 
+COMBINED_FIT
+};
+
+struct PlacementInfo{
+int uldIndex ;
+int spaceIndex;
+int orientation;
+double score;
+double x ,y , z;
+bool placed;
+};
+
+struct PackagePlacement{
+int packageId;
+int uldId;
+double x , y , z;
+double width , height , depth;
+bool placed;
+};
+
+#endif
+
 
