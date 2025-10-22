8d052c47b14766fce6fed69970ebfe2e968ea481
vr10phoenix <vardhanvachana2005@gmail.com>
2025-10-22 18:53:43 +0530

    Update Package.h
    added a fundamental Package Class for our Package container

--- Include/Package.h
+++ Include/Package.h
@@ -1,1 +1,18 @@
+#ifndef PACKAGE_H
+#define PACKAGE_H
+class Package{
+public : 
+  int id;
+  double width , height , depth; 
+  bool isPriority;
 
+ Package(int p_id , double w , double h , double d , bool priority = false);
+
+ double volume() const;
+ void getDimensionsInOrientation( int orientation , double& out_width , 
+                                  double out_height , double& out_depth) const;
+
+ bool operator< (const Package& other) const;
+};
+
+#endif
